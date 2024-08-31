9ccefd851da2180ac3f193957296f0460ca2ba4b
tufankaracam <tufankaracam@hotmail.com.tr>
2024-08-31 19:31:38 +0200

    Updates

--- assets/Message-B0i9nTUc.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CkdRSgI2.js";const a="_message_76u3c_1",t={message:a};function n({text:s}){return e.jsx("div",{className:t.message,children:s})}export{n as M};
---
+++ assets/Message-BRKAT0Mb.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Dhy37DoS.js";const a="_message_2963y_1",t={message:a};function n({text:s}){return e.jsx("div",{className:t.message,children:s})}export{n as M};
